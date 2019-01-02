34fda97222796cdd706253f1fc2b5e26b43ef894
Indre Bakeer <indre.bakeer@gmail.com>
2019-01-02 06:56:33 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,11 +3,8 @@ def oxford_comma(array)
   if array.length == 2
     array.join(" and ")
   else
-    array.length == 3
+    array.length >= 3
     array.join(", "" and ")
-  else 
-    array.length > 3
-    array.join(", , and ")
   end
 end
 
